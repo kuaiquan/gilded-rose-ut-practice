62c556e820025ae21af38a924612a9ef5db78ea4
Seaborn Lee <seabornlee.cn@gmail.com>
2018-06-21 18:06:43 +0800

    Init

---
+++ src/test/java/org/katas/GildedRoseTest.java
@@ -0,0 +1,14 @@
+package org.katas;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GildedRoseTest {
+    @Test
+    public void should_pass() {
+        assertTrue(true);
+    }
+}
